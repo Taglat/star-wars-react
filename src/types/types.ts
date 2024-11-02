4e4229b236d50227ede4c9570bc50f7e3a6be7ff
Talgat <ttokayev7@gmail.com>
2024-11-02 23:30:10 +0500

    Entities Types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,58 @@ export type UserType = {
 }
 
 export type LocalStorageKeys = "email"
+
+export interface IPeople {
+  birth_year: string;
+  eye_color: string;
+  films: string[];
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string | IPlanet;
+  mass: string;
+  name: string;
+  skin_color: string;
+  created: Date;
+  edited: Date;
+  species: string[];
+  starships: string[] | IStarship[];
+  url: string;
+  vehicles: string[];
+}
+export interface IPlanet {
+  climate: string;
+  created: Date;
+  diameter: string;
+  edited: Date;
+  films: string[];
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  residents: string[] | IPeople[];
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+  url: string;
+}
+export interface IStarship {
+  MGLT: string;
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: Date;
+  crew: string;
+  edited: Date;
+  hyperdrive_rating: string;
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  films: string[];
+  pilots: string[] | IPeople[];
+  starship_class: string;
+  url: string;
+}
